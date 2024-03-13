6a2af2492c9f8f76faa3ca120d975fe8e6697473
rubygomez <rvbii.gomez@gmail.com>
2024-03-13 09:35:08 -0700

    separe files for functions

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <div>
+      <footer>
+        <p>Copyright 2024</p>
+      </footer>
+    </div>
+  );
+}
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <div>
+      <header>
+        <a href="#">Home</a> | <a href="#posts-index">All Animals</a> | <a href="#posts-new">New Animals</a>
+      </header>
+    </div>
+  );
+}
